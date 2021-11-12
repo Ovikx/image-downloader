f90c50a5ae0186e5a5d21b21c4304a70ee2ee23c
Ovik <47368700+Ovikx@users.noreply.github.com>
2021-11-11 23:40:32 -0500

    removed unnecessary imports

--- image_downloader.py
+++ image_downloader.py
@@ -1,5 +1,3 @@
-from requests.models import Response
-from requests.sessions import session
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
